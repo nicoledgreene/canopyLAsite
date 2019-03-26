dfe6c07a7c0469c3b1bd38ae2c5c45bd964d759e
Nicole Gilbert <nicolegilbert16@gmail.com>
2019-03-25 21:47:55 -0700

    fix

--- wp-content/themes/canopytheme/header.php
+++ wp-content/themes/canopytheme/header.php
@@ -6,7 +6,7 @@
     <meta name="description" content="A sound and immersion studio in Los Angeles">
 
     <link rel="stylesheet" href="https://use.typekit.net/ysh4zhf.css">
-    <link rel="stylesheet" href="https://use.typekit.net/ysh4zhf.css">>
+    <link rel="stylesheet" href="https://use.typekit.net/ysh4zhf.css">
     <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
     <?php wp_head(); ?>
 </head>
